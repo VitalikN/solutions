ee5796244b89ca45ae9deb6eaad1c6d3ffaa479d
vitalik.nozhenko@gmail.com <109283146+VitalikN@users.noreply.github.com>
2023-10-11 01:09:00 +0300

    Network fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,7 +40,7 @@ export default function Home() {
           selectedItem={selectedItem}
         />
         <Contacts />
-        <Network />
+        {/* <Network /> */}
       </main>
     </>
   );
